4aaf78cc83fe90cd3808b1e00147afd66daeba83
Matthew Leibowitz <mattleibow@live.com>
2021-09-18 23:50:24 +0200

    More ts

--- GitHubWikiEditor/wwwroot/js/site.js
+++ GitHubWikiEditor/wwwroot/js/site.js
@@ -1,41 +1,52 @@
 /// <reference path="../types/simplemde/index.d.ts" />
 /// <reference path="../types/blazor__javascript-interop/index.d.ts" />
+/// <reference path="../types/codemirror/index.d.ts" />
 var Editor = /** @class */ (function () {
     function Editor(editor) {
-        var smde = new SimpleMDE({ element: editor });
-        this.smde = smde;
-        smde.codemirror.on("beforeChange", function (instance, changeObj) {
-            //if (localEdit) {
-            //    return;
-            //}
-            //// We add in line to adjust for paragraph markers
-            //let from = instance.doc.indexFromPos(changeObj.from);
-            //const to = instance.doc.indexFromPos(changeObj.to);
-            //if (from !== to) {
-            //    this.text.removeText(from, to);
-            //}
-            var changedText = changeObj.text;
-            console.log(changedText);
-            DotNet.invokeMethodAsync("GitHubWikiEditor", "SimpleMDE.UpdateTextFromServer", changedText);
-            //changedText.forEach((value, index) => {
-            //    // Insert the updated text
-            //    if (value) {
-            //        this.text.insertText(from, value);
-            //        from += value.length;
-            //    }
-            //    // Add in a paragraph marker if this is a multi-line update
-            //    if (index !== changedText.length - 1) {
-            //        this.text.insertMarker(
-            //            from,
-            //            ReferenceType.Tile,
-            //            { [reservedTileLabelsKey]: ["pg"] });
-            //        from++;
-            //    }
-            //});
+        var _this = this;
+        var smde = new SimpleMDE({
+            element: editor,
+            renderingConfig: {
+                singleLineBreaks: false,
+                codeSyntaxHighlighting: true,
+            }
         });
+        var codemirror = smde.codemirror;
+        this.smde = smde;
+        this.codemirror = codemirror;
+        codemirror.on("beforeChange", function (_, changeObj) { return _this.onBeforeChange(changeObj); });
     }
     Editor.init = function (editor) {
         return new Editor(editor);
+    };
+    Editor.prototype.onBeforeChange = function (changeObj) {
+        if (this.localEdit) {
+            return;
+        }
+        // We add in line to adjust for paragraph markers
+        var doc = this.codemirror.getDoc();
+        var from = doc.indexFromPos(changeObj.from);
+        var to = doc.indexFromPos(changeObj.to);
+        if (from !== to) {
+            //this.text.removeText(from, to);
+        }
+        var changedText = changeObj.text;
+        changedText.forEach(function (value, index) {
+            // Insert the updated text
+            if (value) {
+                //this.text.insertText(from, value);
+                from += value.length;
+            }
+            // Add in a paragraph marker if this is a multi-line update
+            if (index !== changedText.length - 1) {
+                //this.text.insertMarker(
+                //    from,
+                //    ReferenceType.Tile,
+                //    { [reservedTileLabelsKey]: ["pg"] });
+                from++;
+            }
+        });
+        DotNet.invokeMethodAsync("GitHubWikiEditor", "SimpleMDE.UpdateTextFromServer", changedText);
     };
     return Editor;
 }());
--- GitHubWikiEditor/wwwroot/js/site.ts
+++ GitHubWikiEditor/wwwroot/js/site.ts
@@ -1,53 +1,64 @@
 /// <reference path="../types/simplemde/index.d.ts" />
 /// <reference path="../types/blazor__javascript-interop/index.d.ts" />
+/// <reference path="../types/codemirror/index.d.ts" />
 
 class Editor {
     smde: SimpleMDE;
-
-    public constructor(editor) {
-        const smde = new SimpleMDE({ element: editor });
-        this.smde = smde;
-
-        smde.codemirror.on(
-            "beforeChange",
-            (instance, changeObj) => {
-                //if (localEdit) {
-                //    return;
-                //}
-
-                //// We add in line to adjust for paragraph markers
-                //let from = instance.doc.indexFromPos(changeObj.from);
-                //const to = instance.doc.indexFromPos(changeObj.to);
-
-                //if (from !== to) {
-                //    this.text.removeText(from, to);
-                //}
-
-                const changedText = changeObj.text as string[];
-                console.log(changedText);
-
-                DotNet.invokeMethodAsync("GitHubWikiEditor", "SimpleMDE.UpdateTextFromServer", changedText);
-
-                //changedText.forEach((value, index) => {
-                //    // Insert the updated text
-                //    if (value) {
-                //        this.text.insertText(from, value);
-                //        from += value.length;
-                //    }
-
-                //    // Add in a paragraph marker if this is a multi-line update
-                //    if (index !== changedText.length - 1) {
-                //        this.text.insertMarker(
-                //            from,
-                //            ReferenceType.Tile,
-                //            { [reservedTileLabelsKey]: ["pg"] });
-                //        from++;
-                //    }
-                //});
-            });
-    }
+    codemirror: CodeMirror.Editor;
+    localEdit: boolean;
 
     public static init(editor) {
         return new Editor(editor);
+    }
+
+    public constructor(editor) {
+        const smde = new SimpleMDE({
+            element: editor,
+            renderingConfig: {
+                singleLineBreaks: false,
+                codeSyntaxHighlighting: true,
+            }
+        });
+        const codemirror = smde.codemirror as CodeMirror.Editor;
+
+        this.smde = smde;
+        this.codemirror = codemirror;
+
+        codemirror.on("beforeChange", (_, changeObj) => this.onBeforeChange(changeObj));
+    }
+
+    private onBeforeChange(changeObj: CodeMirror.EditorChangeCancellable) {
+        if (this.localEdit) {
+            return;
+        }
+
+        // We add in line to adjust for paragraph markers
+        const doc = this.codemirror.getDoc();
+        let from = doc.indexFromPos(changeObj.from);
+        const to = doc.indexFromPos(changeObj.to);
+
+        if (from !== to) {
+            //this.text.removeText(from, to);
+        }
+
+        const changedText = changeObj.text as string[];
+        changedText.forEach((value, index) => {
+            // Insert the updated text
+            if (value) {
+                //this.text.insertText(from, value);
+                from += value.length;
+            }
+
+            // Add in a paragraph marker if this is a multi-line update
+            if (index !== changedText.length - 1) {
+                //this.text.insertMarker(
+                //    from,
+                //    ReferenceType.Tile,
+                //    { [reservedTileLabelsKey]: ["pg"] });
+                from++;
+            }
+        });
+
+        DotNet.invokeMethodAsync("GitHubWikiEditor", "SimpleMDE.UpdateTextFromServer", changedText);
     }
 }
